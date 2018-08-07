d42c96d6cf5c55f923d5d82cb2e37b2c9d94cf72
Denver Stove <dtstove@gmail.com>
2018-08-07 16:31:29 +1000

    Seeded twice and added the show and index methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "seeding begin"
+
+10.times do
+  article = Article.new({title: Faker::Zelda.item, content: Faker::Overwatch.quote})
+  article.save!
+  puts article
+end
+
+puts "seeding end"
