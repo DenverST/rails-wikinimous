20851621ae6495679f71f3ff5a8e7ddfdd6fdb01
Denver Stove <dtstove@gmail.com>
2018-08-07 16:55:07 +1000

    Finished All of the CRUD things

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,18 +9,38 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path(@article)
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    @article.save!
+    redirect_to articles_path(@article)
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:article).permit(:title, :content)
   end
 
 end
